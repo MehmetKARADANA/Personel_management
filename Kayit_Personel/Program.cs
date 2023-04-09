9bb26dca02d52df006f52374b26b0c1c0b6fdcab
Mehmet Karadana <karadanam519@gmail.com>
2023-04-09 17:19:19 +0300

    kullanıcı yetkilendirme test pushlama

--- Kayit_Personel/Program.cs
+++ Kayit_Personel/Program.cs
@@ -8,9 +8,6 @@ namespace Kayit_Personel
 {
     internal static class Program
     {
-        /// <summary>
-        /// Uygulamanın ana girdi noktası.
-        /// </summary>
         [STAThread]
         static void Main()
         {
